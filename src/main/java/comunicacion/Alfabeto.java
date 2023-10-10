5789afdf376c643405e71ade28cf580018300947
= <60821161+Agua-bit@users.noreply.github.com>
2023-10-10 00:48:22 -0500

    R

--- src/main/java/comunicacion/Alfabeto.java
+++ src/main/java/comunicacion/Alfabeto.java
@@ -1,2 +1,28 @@
-package comunicacion;public class Alfabeto {
+package comunicacion;
+
+public class Alfabeto extends Pictograma{
+
+    private String[] letras;
+    private String interpretacion;
+
+    public Alfabeto(String[] letras, String interpretacion) {
+        this.letras = letras;
+        this.interpretacion = interpretacion;
+    }
+
+    public String[] getLetras() {
+        return letras;
+    }
+
+    public void setLetras(String[] letras) {
+        this.letras = letras;
+    }
+
+    public String getInterpretacion() {
+        return interpretacion;
+    }
+
+    public void setInterpretacion(String interpretacion) {
+        this.interpretacion = interpretacion;
+    }
 }
--- src/main/java/comunicacion/Fabula.java
+++ src/main/java/comunicacion/Fabula.java
@@ -1,2 +1,29 @@
-package comunicacion;public class Fabula {
+package comunicacion;
+
+public class Fabula extends Escrito{
+
+    private String ensenanza;
+    private String interpretacion;
+
+    public Fabula(String origen, String titulo, String autor, int paginas, String ensenanza, String interpretacion) {
+        super(origen, titulo, autor, paginas);
+        this.ensenanza = ensenanza;
+        this.interpretacion = interpretacion;
+    }
+
+    public String getEnsenanza() {
+        return ensenanza;
+    }
+
+    public void setEnsenanza(String ensenanza) {
+        this.ensenanza = ensenanza;
+    }
+
+    public String getInterpretacion() {
+        return interpretacion;
+    }
+
+    public void setInterpretacion(String interpretacion) {
+        this.interpretacion = interpretacion;
+    }
 }
